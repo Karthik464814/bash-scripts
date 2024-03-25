ba0211e14bcb3b47388c1c44db1967c3c9dd8701
karthi <113457921+Karthik464814@users.noreply.github.com>
2024-03-25 17:12:45 +0530

    Add files via upload

---
+++ greetings.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "Hi there, what is your name?"
+
+read name
+
+echo "That's a wonderful you have got there."
+
+sleep 2
+
+echo "$name, are you currently studying or working? "
+
+sleep 2
+
+read current
+
+sleep 2
+
+echo "Oh.. that's great.!!!"
+
+sleep 2
+
+echo "you have a  great smile, $name"
+
+sleep 2
+
+echo "And where are you from?"
+
+read place
+
+sleep 2
+
+echo "Okay, then it's nice seeing you $name, we will catch up on when i visit $place"
+
+sleep 2
+
